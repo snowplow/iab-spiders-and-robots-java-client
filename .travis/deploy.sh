b12a6015b7881c57b3efeb87bc3ab2af844e1746
Valdimir Ladynev <v.ladynev@gmail.com>
2017-03-30 02:25:14 +0300

    fixed deploy.sh for travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,10 +5,10 @@ tag_version=$1
 cd $TRAVIS_BUILD_DIR
 pwd
 
-project_version=`./gradlew printVersion`
+project_version=`./gradlew -q printVersion`
 if [ "${project_version}" == "${tag_version}" ]; then
     ./gradlew bintrayUpload
 else
-    echo "Tag version '${tag_version}' doesn't match version in android project ('${project_version}'). Aborting!"
+    echo "Tag version '${tag_version}' doesn't match version in the project ('${project_version}'). Aborting!"
     exit 1
 fi
